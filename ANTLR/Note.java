097fa60fc18256ab716a051620eea39876b4cf02
António Santos <aj.santos@ua.pt>
2018-06-21 17:46:30 +0100

    Add Note.java

--- ANTLR/Note.java
+++ ANTLR/Note.java
@@ -1,8 +1,61 @@
 public class Note {
-	String notation;
-	int value;
+	private String notation;
+	private int value;
+	private double duration;
 
-	public Note(String n) {
+	public Note(String note) {
+		duration = 1;
+		notation = note;
 		
+		value = getBaseValue(note.charAt(0));
+
+		String noDuration = note;
+		if (note.contains("{")) {
+			duration = Double.parseDouble(note.substring(note.indexOf("{") + 1, note.indexOf("}")));
+			noDuration = note.substring(0, note.indexOf("{"));
+		}
+		
+		if(noDuration.matches(".*[0-8].*")) {
+			int octave = Character.getNumericValue(noDuration.charAt(noDuration.length() - 1));
+			value += 12*octave;
+		} else {
+			value += 48; // 48 = 12 * 4, 4 is the octave by default
+		}
+		
+
+		String[] splitted = noDuration.split("");
+		for (int i = 1; i < splitted.length - 1; i++) {
+			if(splitted[i].equals("#"))
+				value++;
+			else // equals "b"
+				value--;
+		}
+	}
+
+	private int getBaseValue(char symbol) {
+		
+		switch (symbol) {
+			case 'C':
+				return 12;
+			case 'D':
+				return 14;
+			case 'E':
+				return 16;
+			case 'F':
+				return 17;
+			case 'G':
+				return 19;
+			case 'A':
+				return 21;
+			case 'B':
+				return 23;
+		}
+
+		return -1;
+	}
+
+	@Override
+	public String toString() {
+		return "Note [notation=" + notation + ", value=" + value + ", duration=" + duration + "]";
 	}
 }
