4a4cb3e48284b076f1dd6eddf9b888ec1402c551
Antonio <aj.santos@ua.pt>
2018-06-25 12:21:56 +0100

    retweak Performance.java

--- ANTLR/Performance.java
+++ ANTLR/Performance.java
@@ -1,12 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Performance {
-	private double startTime;
+	private double[] startTime;
 	private Sequence sequence;
 	private Instrument instrument;
 	private int repeatTimes;
 
 	// constructors
+	public Performance(double[] startArray, Sequence seq, Instrument inst, int repeat){
+		startTime = startArray;
+		sequence = seq;
+		instrument = inst;
+		repeatTimes = repeat;
+	}
 	public Performance(double start, Sequence seq, Instrument inst, int repeat) {
-		startTime = start;
+		startTime = startToArray(start);
 		sequence = seq;
 		instrument = inst;
 		repeatTimes = repeat;
@@ -19,10 +28,19 @@ public class Performance {
         this(p.startTime, new Sequence(p.sequence), p.instrument, p.repeatTimes);
     }
 
+
+    //Função para converter um double num Array de double's
+    private double[] startToArray(double start){
+    	double[] tmpArray = new double[0];
+		tmpArray[0] = start;
+    	return tmpArray;
+    }
+
 	// getters & setters
 	public double duration() {
 		return sequence.duration();
 	}
+	//return array sequencial( 0-1-5-6-23-2123-...)
 	public double[] startTime() {
 		return startTime;
 	}
@@ -32,13 +50,30 @@ public class Performance {
 	// TODO!! 2 functions bellow + startTime() which should return more than 1 value, if applicable
 	// repeats (2+ equal start times) must be supported!
 	public void changeStartTime(double toReplace, double newStart) {
-		assert newStart > 0;
-		//startTime = start;
-		startTime = newStart;
+		assert newStart > 0 && (new ArrayList<>(Arrays.asList(startTime))).contains(toReplace);
+
+		System.out.println("Linha 55 Performance: " + (new ArrayList<>(Arrays.asList(startTime))).contains(toReplace));
+
+		for(int i = 0; i < startTime.length; i++){
+			if(startTime[i] == toReplace){
+				startTime[i] = newStart;
+				break;
+			}
+		}
+
+		Arrays.sort(startTime);
 	}
 	public void addStartTime(double start) {
 		assert start > 0;
-		//startTime = start;
+		double[] tmp = new double[startTime.length + 1];
+		for(int i = 0; i < startTime.length; i++){
+			tmp[i] = startTime[i];
+		}
+		tmp[startTime.length] = start;
+		startTime = tmp;
+
+		Arrays.sort(startTime);
+
 	}
 	public void repeatTimes(int repeat) {
 		assert repeat > 0;
@@ -51,9 +86,6 @@ public class Performance {
 	}
 	public Performance modulateTempo(double tempoChange) {
 		return new Performance(startTime, sequence.modulateTempo(tempoChange), instrument, repeatTimes);
-	}
-	public Performance delay(double delay) {
-		return new Performance(startTime + delay, sequence, instrument, repeatTimes);
 	}
 	public Performance repeat(int repeats) {
 		return new Performance(startTime, sequence, instrument, repeats);
