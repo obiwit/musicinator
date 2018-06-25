dc301fe434ea1d63793da57a84074dc3bc6d6b73
Tomás Freitas <tomasfreitas@ua.pt>
2018-06-25 15:07:12 +0100

    small changes to template

--- test3.py
+++ test3.py
@@ -8,7 +8,7 @@ vartrack = 2 #to be defined by user
 
 
 ####
-toadd = [[1,10,15,25,36],(64,0.5,2),(62,0.25,25),1]
+toadd = [[1,10,15,25,36],(64,0.5,2,3),(62,0.25,25,4),1]
 
 midi = MIDIFile(numTracks=1500, file_format=1) #it takes the number of tracks as a parameter
 midi.addTempo(0,0,bpm) #adding tempo
@@ -26,14 +26,14 @@ def addnotes(notes):
     repeat_times = (int)(notes[len(notes)-1])
     
     for x in range(len(time)):
-        currtime = time[x]
+        initialTime = time[x]
         for _ in range(repeat_times):
             for i in range(1, len(notes)-1):
                 try:
                     note = notes[i][0] #getting the note
                     duration = notes[i][1] #duration of the note
-                    if note < 0:
-                        currtime+=duration
+                    toInsert = notes[i][3] + initialTime
+                    if note == -1:
                         continue
 
                 except:
@@ -42,16 +42,15 @@ def addnotes(notes):
                 try: #trying for instrument change
                     instrument = notes[i][2] #checking for instrument
                     if(instrument != temp):
-                        midi.addProgramChange(currtrack, 0, currtime, instrument) #changing the current instrument
+                        midi.addProgramChange(currtrack, 0, toInsert, instrument) #changing the current instrument
                         temp = instrument
                 except:
                     instrument = temp #it keeps the old instrument
                     pass
                 
-                midi.addNote(currtrack,0,note,currtime,duration,100)
+                midi.addNote(currtrack,0,note,toInsert,duration,100)
 
-                print("Added note {}, with instument {} with a duration of {} on time {}, on channel {}".format(note, instrument, duration, currtime, currtrack))
-                currtime += duration
+                print("Added note {}, with instument {} with a duration of {} on time {}, on channel {}".format(note, instrument, duration, toInsert, currtrack))
         currtrack += 1
 
 
