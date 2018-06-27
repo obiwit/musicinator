1e662ca4a9f8c200e5bec0a54559f7f665e276e3
Antonio <aj.santos@ua.pt>
2018-06-27 14:09:13 +0100

    Fix what have been fixed

--- Visitors/Instrument.java
+++ Visitors/Instrument.java
@@ -10,9 +10,10 @@ public class Instrument {
         value = new int[NOTERANGE];        	
         for (int i = 0; i < NOTERANGE; i++)        	
             value[i] = v;        	
-    }		
+    }
+
     public void concatenate(Instrument inst, int discriminant){	
-        for(int i = 1; i < NOTERANGE; i++){	
+        for(int i = 0; i < NOTERANGE; i++){	
             int val = inst.value(i);	
             if(val != discriminant){	
                 value[i] = val;	
