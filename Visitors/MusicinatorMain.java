414f97f651f8a091e203fe46a90d327c8ab2dc53
Tomás Freitas <tomasfreitas@ua.pt>
2018-06-28 15:32:50 +0100

    changes to Main Class

--- Visitors/MusicinatorMain.java
+++ Visitors/MusicinatorMain.java
@@ -4,77 +4,96 @@ import org.antlr.v4.runtime.tree.*;
 import java.io.*;
 
 public class MusicinatorMain {
-   public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+        ErrorHandler errors = new ErrorHandler("main.txt");
+        boolean debugflag = false;
+        // looking for files
+        int size = args.length;
+        String muxName = null, auxName = null;
+        for(int i = 0; i < size; i++){
+            if(args[i].contains(".mux")){
+                muxName = args[i];
+            }
+            if(args[i].contains("-d") || args[i].contains("--debug")){
+                debugflag = true;
+            }
+            if(args[i].contains(".aux")){
+                auxName = args[i];
+            }
+        }
 
-      // get input streams for aux and main files
-      InputStream aux_in = null, main_in = null;
-      try { 
-         aux_in = new FileInputStream(new File(args[0])); 
-      }
-      catch(FileNotFoundException e) {
-         System.err.println("ERROR: reading aux file!"); 
-         System.exit(1);
-      }
-      catch(ArrayIndexOutOfBoundsException e) {
-         System.err.println("ERROR! Usage: java MusicinatorMain <aux file> <main file>"); 
-         System.exit(1);
-      }
-      try {
-         main_in = new FileInputStream(new File(args[1]));
-      }
-      catch(FileNotFoundException e) {
-         System.err.println("ERROR: reading main file!"); 
-         System.exit(1);
-      }
-      catch(ArrayIndexOutOfBoundsException e) {
-         System.err.println("ERROR! Usage: java MusicinatorMain <aux file> <main file>"); 
-         System.exit(1);
-      }
-
-      // generate output filename
-      String filename =  args[1].split("\\.")[0] + ".py";
-      if (args.length > 2)
-         filename = args[2] + ".py";
-
-      // create a parser from the aux file
-      CharStream aux_input = CharStreams.fromStream(aux_in);
-      AuxinatorLexer aux_lexer = new AuxinatorLexer(aux_input);
-      CommonTokenStream aux_tokens = new CommonTokenStream(aux_lexer);
-      AuxinatorParser aux_parser = new AuxinatorParser(aux_tokens);
-
-      // create a parser from the main file
-      CharStream main_input = CharStreams.fromStream(main_in);
-      MusicinatorLexer main_lexer = new MusicinatorLexer(main_input);
-      CommonTokenStream main_tokens = new CommonTokenStream(main_lexer);
-      MusicinatorParser main_parser = new MusicinatorParser(main_tokens);
-
-      // begin parsing at main rule
-      ParseTree aux_tree = aux_parser.main();
-      ParseTree main_tree = main_parser.main();
-
-      if (aux_parser.getNumberOfSyntaxErrors() == 0 && 
-         main_parser.getNumberOfSyntaxErrors() == 0) {
-
-         // walk aux file's tree
-         // ParseTreeWalker aux_walker = new ParseTreeWalker();
-         // AuxPhase aux = new AuxPhase();
-         // aux_walker.walk(aux, aux_tree);
-         AuxVisitor aux = new AuxVisitor();
-         aux.visit(aux_tree);
-
-
-         // visit main file's tree;
-         SemanticAnalysis semanticVisitor = new SemanticAnalysis(aux.music);
-         semanticVisitor.visit(main_tree);
-
-         // check there were no semantic errors
-         if (semanticVisitor.errors.errorsDetected()) {
+        if(muxName == null){
+            errors.error("ERROR! Usage: java MusicinatorMain <main file> [aux file] [-d | --debug] ");
             System.exit(1);
-         }
-         
-         // start compilation
-         Compiler compilerVisitor = new Compiler(aux.music, semanticVisitor.globalScope, filename);
-         compilerVisitor.visit(main_tree);
-      }
-   }
+        }
+        if(auxName == null){
+            Music m = new Music();
+        }
+
+
+        // get input streams for aux and main files
+        InputStream aux_in = null, main_in = null;
+        if(auxName != null){
+            try {
+                aux_in = new FileInputStream(new File(auxName));
+            } catch (FileNotFoundException e) {
+                errors.error("ERROR: reading aux file!");
+                System.exit(1);
+            }
+        }
+        try {
+            main_in = new FileInputStream(new File(muxName));
+        } catch (FileNotFoundException e) {
+            errors.error("ERROR: reading main file!");
+            System.exit(1);
+        }
+
+        // generate output filename
+        String filename = muxName.split("\\.")[0];
+        
+
+        // create a parser from the aux file
+        CharStream aux_input = CharStreams.fromStream(aux_in);
+        AuxinatorLexer aux_lexer = new AuxinatorLexer(aux_input);
+        CommonTokenStream aux_tokens = new CommonTokenStream(aux_lexer);
+        AuxinatorParser aux_parser = new AuxinatorParser(aux_tokens);
+
+        // create a parser from the main file
+        CharStream main_input = CharStreams.fromStream(main_in);
+        MusicinatorLexer main_lexer = new MusicinatorLexer(main_input);
+        CommonTokenStream main_tokens = new CommonTokenStream(main_lexer);
+        MusicinatorParser main_parser = new MusicinatorParser(main_tokens);
+
+        // begin parsing at main rule
+        ParseTree aux_tree = aux_parser.main();
+        ParseTree main_tree = main_parser.main();
+
+        if (aux_parser.getNumberOfSyntaxErrors() == 0 && main_parser.getNumberOfSyntaxErrors() == 0) {
+
+            // Visitor
+            AuxVisitor aux = new AuxVisitor();
+            aux.visit(aux_tree);
+
+            // visit main file's tree;
+            if(auxName != null){
+                SemanticAnalysis semanticVisitor = new SemanticAnalysis(aux.music);
+            }else{
+                SemanticAnalysis semanticVisitor = new SemanticAnalysis(m);
+            }
+            semanticVisitor.visit(main_tree);
+
+            // check there were no semantic errors
+            if (semanticVisitor.errors.errorsDetected()) {
+                System.exit(1);
+            }
+
+            // start compilation
+            if(auxName != null){
+                Compiler compilerVisitor = new Compiler(aux.music, semanticVisitor.globalScope, filename);
+            }else{
+                Compiler compilerVisitor = new Compiler(m, semanticVisitor.globalScope, filename);
+            }
+            compilerVisitor.visit(main_tree);
+        }
+    }
 }
